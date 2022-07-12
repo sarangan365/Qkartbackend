4f1a34351d7944e414501b5523438f3ba1fa0fa7
CrioUser <crio-user@criodo.com>
2022-07-12 17:11:58 +0530

    modified the ENV and setupRemote.sh

--- setupRemoteMongo.sh
+++ setupRemoteMongo.sh
@@ -1,3 +1,3 @@
 # Setup file template to upload data to MongoDB Atlas
-mongoimport --uri <add-url-connection-string-here> --drop --collection users --file data/export_qkart_users.json
-mongoimport --uri <add-url-connection-string-here> --drop --collection products --file data/export_qkart_products.json
+mongoimport --uri "mongodb+srv://sarangan365:sECURE@2711@qkart-node.akwure3.mongodb.net/qkart?retryWrites=true&w=majority" --drop --collection users --file data/export_qkart_users.json
+mongoimport --uri "mongodb+srv://sarangan365:sECURE@2711@qkart-node.akwure3.mongodb.net/qkart?retryWrites=true&w=majority" --drop --collection products --file data/export_qkart_products.json
